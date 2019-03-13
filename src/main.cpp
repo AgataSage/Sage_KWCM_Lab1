60af23f2ac44725d5a3e378c5cd3aaad063ed67a
Student <student@ib.polsl.pl>
2019-03-13 10:02:48 +0100

    drugi commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #pragma once
+#include "Trygonometria\trygonometria.h"
+#include <iostream>
 
 int main() { 
-	/*...*/ 
+	/*...*/
+	double resultSin = degreemath::Sin(90.0);
+	double resultCos = degreemath::Cos(0.0);
+	double resultTan = degreemath::Tan(45.0);
+	double resultCtan = degreemath::Ctan(45.0);
+
+	std::cout << "sin: " << resultSin << std::endl;
+	std::cout << "cos: " << resultCos << std::endl;
+	std::cout << "tan: " << resultTan << std::endl;
+	std::cout << "ctan: " << resultCtan << std::endl;
+
 	return 0; 
 }
